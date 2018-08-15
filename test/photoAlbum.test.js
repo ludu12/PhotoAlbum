c37aeb3d8855314750d419eb1f86e9672774ba1b
Robert <robert.dunscombe@siemens.com>
2018-08-15 09:08:28 -0500

    Changed some logic of tests

--- test/photoAlbum.test.js
+++ test/photoAlbum.test.js
@@ -1,26 +1,33 @@
-const expect = require('chai').expect;
-var nock = require('nock');
+var chai = require('chai'),
+    expect = chai.expect,
+    sinonChai = require('sinon-chai'),
+    sinon = require('sinon'),
+    nock = require('nock');
+
+chai.use(sinonChai);
+
 const { getByAlbumId } = require('../photoAlbum');
 
-var albumIdRes = [
-  {
-    'albumId': 3,
-    'id': 1,
-    'title': 'Title1',
-    'url': 'http://url1.com',
-    'thumbnailUrl': 'http://thumbnail1.com'
-  },
-  {
-    'albumId': 3,
-    'id': 2,
-    'title': 'Title2',
-    'url': 'http://url2.com',
-    'thumbnailUrl': 'http://thumbnail2.com'
-  }
-]
-
 describe('Get By Id tests', () => {
+  var albumIdRes = [
+    {
+      'albumId': 3,
+      'id': 1,
+      'title': 'Title1',
+      'url': 'http://url1.com',
+      'thumbnailUrl': 'http://thumbnail1.com'
+    },
+    {
+      'albumId': 3,
+      'id': 2,
+      'title': 'Title2',
+      'url': 'http://url2.com',
+      'thumbnailUrl': 'http://thumbnail2.com'
+    }
+  ]
+  
   beforeEach(() => {
+    sinon.spy(console, 'log');
     // Success mock
     nock('https://jsonplaceholder.typicode.com')
       .get('/photos?albumId=3')
@@ -32,15 +39,18 @@ describe('Get By Id tests', () => {
       .replyWithError('ERROR!');
   });
 
+  afterEach(function() {
+    console.log.restore();
+  });
+
   it('Should get photos by Album Id', () => {
-    return getByAlbumId(3)
+    getByAlbumId(3)
       .then(response => {
         expect(response).to.deep.equal(albumIdRes);
       });
   });
 
   it('Should report error when there is one', () => {
-    return getByAlbumId(-1);
-    expect( console.log.calledOnce ).to.be.true;
+    getByAlbumId(-1).then(() => {expect(console.log).to.be.called;});
   });
 });
